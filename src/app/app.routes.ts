75db8a904ac5f98b03c88468b6b4bc2f97acbfc9
veritozy <verozy26@gmail.com>
2026-07-07 20:46:22 -0500

    agregando ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { Libros } from './features/libros/libros';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'libros', pathMatch: 'full' },
     {path:'libros', component:Libros}
 ];
